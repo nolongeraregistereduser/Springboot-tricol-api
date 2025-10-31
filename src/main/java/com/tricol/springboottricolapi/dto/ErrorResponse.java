88ff3893cb35439f71c9fafad9216a70c6f57088
nolongeraregistereduser <mohamedzouhairi023@gmail.com>
2025-10-31 11:19:07 +0100

    STA-7 adding error reponse dto

---
+++ src/main/java/com/tricol/springboottricolapi/dto/ErrorResponse.java
@@ -0,0 +1,26 @@
+package com.tricol.springboottricolapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private LocalDate timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+
+
+
+
+
+}
